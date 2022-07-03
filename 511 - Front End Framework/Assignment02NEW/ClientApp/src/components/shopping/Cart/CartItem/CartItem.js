fa379ac1aa4f72746fdceaadd6b6c78edd3b3404
Simon Dionne-Couture <sdionne1201@hotmail.com>
2022-07-02 23:22:38 -0400

    Assignment02 finally added MISSING FETCH

---
+++ 511 - Front End Framework/Assignment02NEW/ClientApp/src/components/shopping/Cart/CartItem/CartItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export class CartItem extends React.Component{
+
+    render(){
+        return (
+            <div className='cartItem-container'>
+                <h1>This is the Cart Item</h1>
+            </div>
+        )
+    }
+
+    // getCartItem(){
+
+    // }
+}
+
+export default CartItem;
